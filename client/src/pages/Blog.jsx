aa869525a7611214a051cae1f2206170b48d8413
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-05-28 20:15:58 +0530

    Created more components e.g BlogCard,Footer,Newslette,etc

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { assets, blog_data } from "../assets/assets";
+import { Navbar } from "../components/Navbar";
+import moment from "moment";
 
 export const Blog = () => {
-  return (
-    <div>
-        <h1>Blog</h1>
+  const { id } = useParams();
+  const [data, setData] = useState(null);
+  // const fetchBlogData = async ()=> {
+  //   blog_data.find(item => item._id == id)
+  //   setData(data)
+  // }
+  const fetchBlogData = async () => {
+    const blog = blog_data.find((item) => item._id == id);
+    setData(blog);
+  };
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+  return data ? (
+    <div className="relative">
+      <img
+        src={assets.gradientBackground}
+        alt=""
+        className="absolute -top-50 -z-1 opacity-50"
+      />
+      <Navbar />
+      {/* <div>
+          <p>Published on {moment(data.createdAt).format('MMMM Do YYYY')}</p>
+          </div> */}
+      <div className="text-center mt-20 text-gray-600">
+        <p className="text-primary py-4 font-medium">
+          Published on {moment(data.createdAt).format("MMMM Do YYYY")}
+        </p>
+
+        <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">
+          {data.title}
+        </h1>
+
+        <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
+
+        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
+          Michael Brown
+        </p>
+      </div>
+
+{/* ------------------------ */}
+      <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
+          <img
+          src={data.image}
+          alt=""
+          className='rounded-3xl mb-5'
+         />
+
+  <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML=
+  {{ __html: data.description }}> </div>
+
+  {/* Comments Section */}
+  <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+    <p>Comments</p>
+  </div>
+</div>
+
+{/* ------------------------- */}
     </div>
-  )
-}
+  ) : (
+    <div>Loading...</div>
+  );
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 import Header from "../components/Header";
 import { BlogList } from "../components/BlogList";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 
 const Home = () => {
@@ -10,6 +12,8 @@ const Home = () => {
       <Navbar/>
       <Header/>
       <BlogList/>
+      <Newsletter/>
+      <Footer/>
     </div>
   )
 };
